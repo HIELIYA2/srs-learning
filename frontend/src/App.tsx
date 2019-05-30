7b18d5cc2f6d06db7dd41628aeb96a000133c816
Eliyahu hillel <hillel.eli396@gmail.com>
2019-05-30 06:58:22 +0300

    try to display string from server

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './assets/css/app.css';
-
 import Router from './RouterCmp';
 
-function App() {
-    return <Router />;
+const axios = require('axios');
+
+interface State {
+    masage: string;
 }
+
+class App extends React.Component<State> {
+    constructor(Props: Readonly<State>) {
+        super(Props);
+        this.state = {
+            masage: '',
+        };
+    }
+    componentDidMount() {
+        axios.get('http://localhost:3000').then((res: any) => {
+            console.log(res);
+            this.setState({ masage: res.data });
+        });
+    }
+
+    render() {
+        // let masage = this.state;
+        console.log(this.state);
+
+        return (
+            <div className="home-page">
+                <h1 className="home-title">app page</h1>
+                {/* <h1 className="home-title">{this.state.masage}</h1> */}
+                <Router />;
+            </div>
+        );
+    }
+}
+// function App() {
+//     return <Router />;
+// }
 
 export default App;
