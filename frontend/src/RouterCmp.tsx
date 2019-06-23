0aac225ee48e9add29f5577f9b96da373bfbf1d2
Eliyahu hillel <hillel.eli396@gmail.com>
2019-06-24 01:06:00 +0300

    nav bar component

--- frontend/src/RouterCmp.tsx
+++ frontend/src/RouterCmp.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 //Pages
-import Nav from './cmps/nav/nav';
+import Nav from './cmps/layout/navbar/NavBar';
 import Home from './pages/home/home';
 import Create from './pages/create/create';
 import Learn from './pages/learn/learn';
