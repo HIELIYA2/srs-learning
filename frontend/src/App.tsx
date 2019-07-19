baf345fd762b2d2b21e0f6c116f3918edbc674b4
Eliyahu hillel <hillel.eli396@gmail.com>
2019-07-19 09:20:09 +0300

    fix app + index (main)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -22,18 +22,12 @@ class App extends React.Component<State> {
     }
 
     render() {
-        // let masage = this.state;
-        console.log(this.state);
         return (
             <div className="home-page">
-                {/* <h1 className="home-title">{this.state.masage}</h1> */}
                 <Router />
             </div>
         );
     }
 }
-// function App() {
-//     return <Router />;
-// }
 
 export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import reducer from './reducers/index';
-const store = createStore(reducer);
+import store from './store';
 
 ReactDOM.render(
     <Provider store={store}>
